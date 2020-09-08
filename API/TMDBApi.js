008ec7ff06761fd435613301a14718bddc67cace
AchrafNAMIRI <Achrafnamiri17@gmail.com>
2020-09-08 17:03:59 +0200

    initial commit

---
+++ API/TMDBApi.js
@@ -0,0 +1,23 @@
+
+
+
+ const API_TOKEN = "7c8a1b63d440fc011001e220d15df8d5";
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+
+  }
+  // API/TMDBApi.js
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
+
+  // RÃ©cupÃ©ration du dÃ©tail d'un film
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
